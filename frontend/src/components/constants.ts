149f5730d350af72ade80eb912dae2f89a25c5a3
faujdardevansh <faujdardevansh@gmail.com>
2026-01-03 22:49:24 +0530

    feat: implement donation form with title, description, and category selection

---
+++ frontend/src/components/constants.ts
@@ -0,0 +1,6 @@
+export const DonationCategory = [
+  { value: "FOOD", label: "Food" },
+  { value: "CLOTHES", label: "Clothes" },
+  { value: "SUPPLIES", label: "Supplies" },
+  { value: "OTHER", label: "Other" },
+];
--- frontend/src/pages/Donation.tsx
+++ frontend/src/pages/Donation.tsx
@@ -1,10 +1,55 @@
-import TotalDonation from "../components/DonationDashboardComponents/TotalDonation";
+import { Form, Input, Card, Button, Row, Col, Select } from "antd";
+import { DonationCategory } from "../components/constants";
 
 const Donation = () => {
   return (
-    <div className="min-h-screen rounded-md border-white border-2 m-4">
-      <TotalDonation />
-      <div></div>
+    <div className="min-h-screen m-4">
+      <Card
+        className="w-full "
+        title={<h1 className="text-xl font-semibold">Create a Donation</h1>}
+      >
+        <Form layout="vertical">
+          <Row gutter={24}>
+            <Col span={8}>
+              <Form.Item
+                name="title"
+                label="Title"
+                rules={[
+                  {
+                    required: true,
+                    message: "Title is required",
+                  },
+                ]}
+              >
+                <Input placeholder="Title" />
+              </Form.Item>
+            </Col>
+
+            <Col span={8}>
+              <Form.Item name="description" label="Description">
+                <Input placeholder="Description" />
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              {" "}
+              <Form.Item
+                name="category"
+                label="Category"
+                rules={[{ required: true, message: "Category is required" }]}
+              >
+                <Select
+                  placeholder="Select category"
+                  options={DonationCategory}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Form.Item>
+            <Button type="primary">Submit</Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 };
