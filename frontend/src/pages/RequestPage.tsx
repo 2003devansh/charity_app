1b8cae78f73875c3971ca6ec97aadbee1c1b34ba
faujdardevansh <faujdardevansh@gmail.com>
2026-01-21 18:50:32 +0530

    message

--- frontend/src/pages/RequestPage.tsx
+++ frontend/src/pages/RequestPage.tsx
@@ -1,5 +1,5 @@
 const RequestPage = () => {
-  return <div>Request PAge (wil continue work tommorow) qwertyui</div>;
+  return <div>Request PAge (wil continue work tommorow fghjk) qwertyui</div>;
 };
 
 export default RequestPage;
