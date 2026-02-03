f3d5f55681332e3387dfda363f50e19ff3da8c93
faujdardevansh <faujdardevansh@gmail.com>
2026-02-03 21:10:20 +0530

    Update Dashboard component to include additional context

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,9 @@
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  return (
+    <div>
+      Dashboard Page , this is the dashboard component , gonna do work soon{" "}
+    </div>
+  );
 };
 
 export default Dashboard;
